76be9c26aba1517d23f359babf0c2e7979ee06ef
Koro0 <ledane92@gmail.com>
2022-09-09 02:06:17 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGOOSE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
