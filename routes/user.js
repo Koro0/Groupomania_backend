754b3eb619c58552580a1e7347ac7187a4d51f83
Koro0 <ledane92@gmail.com>
2022-12-30 14:16:58 +0100

    add userRoute for get name and admin

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-
+const auth = require('../middleware/auth');
 //////// appel au Ctrl /////////////////////////////
 const userCtrl = require('../controllers/user');
 
 router.post('/signup', userCtrl.signUp);
 router.post('/login', userCtrl.logIn);
-router.get('/pseudo', userCtrl.pseudo);
+
+router.get('/user/:id', auth, userCtrl.getPseudo);
+router.get('/admin/:id', auth, userCtrl.checkAdmin);
+
 module.exports = router;
