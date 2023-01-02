cd51721bc2e24f8fa52cdfaa2d74ddf993f65151
Koro0 <ledane92@gmail.com>
2023-01-02 01:10:57 +0100

    delete imageUrl required

--- models/Post.js
+++ models/Post.js
@@ -4,7 +4,7 @@ const postSchema = mongoose.Schema({
   userId: { type: String, required: true },
   title: { type: String, required: true },
   description: { type: String, required: true },
-  imageUrl: { type: String, required: true },
+  imageUrl: { type: String },
   likes: { type: Number, required: true },
   usersLiked: { type: [String], required: true },
   comments: { type: [Object] },
